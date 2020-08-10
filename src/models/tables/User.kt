567a78d725d91aa75cf577ae250ec9c2fc9a439c
payam karamvandi <payam.elm@gmail.com>
2020-08-10 04:43:02 +0430

    change User superType from Table to IntIdTable

--- src/models/tables/User.kt
+++ src/models/tables/User.kt
@@ -1,11 +1,12 @@
 package com.example.models.tables
 
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Table
 
 /**
  *     Top abstract User class that each user type will implement respectively.
  */
-abstract class User :Table() {
+abstract class User :IntIdTable() {
     val email = varchar("email", 320)
     val username = varchar("username", 320)
     val passwordHash = varchar("passwordHash", 64)
