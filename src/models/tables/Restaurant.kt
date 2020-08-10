53111a37f87d8be1458c06b1d6c2cd9180b69300
payam karamvandi <payam.elm@gmail.com>
2020-08-10 04:42:11 +0430

    Adding acceptRecipe column to Restaurant table

--- src/models/tables/Restaurant.kt
+++ src/models/tables/Restaurant.kt
@@ -5,5 +5,6 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 object Restaurant : IntIdTable() {
     val name = varchar("name", 255)
     val address = varchar("address", 500)
+    val acceptRecipe= bool("acceptRecipe")
 }
 
