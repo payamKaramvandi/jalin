f89367eb1fad9b182c39622d3d912aa47ef63e5f
payam karamvandi <payam.elm@gmail.com>
2020-08-10 23:43:19 +0430

    Adding Order table

---
+++ src/models/tables/Order.kt
@@ -0,0 +1,11 @@
+package com.example.models.tables
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object Order : IntIdTable() {
+    var customerId = reference("customerId", Customer)
+    var orderDate = varchar("date", 12)
+    var paymentType = varchar("paymentType", 12)
+    var total = long("total")
+    var status = varchar("status", 12)
+}
